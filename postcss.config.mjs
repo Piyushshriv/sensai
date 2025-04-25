5338fc83d0875dc5b7aa3348e8e884ce972aca32
Piyushshriv <160409807+Piyushshriv@users.noreply.github.com>
2025-04-25 19:18:31 +0530

    Your commit message

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,9 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcss(),   // Ensure you're using the correct version of tailwindcss here
+    autoprefixer(),
+  ],
 };
